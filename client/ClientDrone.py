9af07cb483d1302b82437634d3db3e430bdbb1e8
alemonteleale <96069240+alemonteleale@users.noreply.github.com>
2021-12-21 11:07:07 +0100

    drone and the diagram
    swag mickle around the world

--- client/ClientDrone.py
+++ client/ClientDrone.py
@@ -2,27 +2,45 @@ import requests
 import random
 import schedule
 from datetime import datetime
+import time 
 
-def dronesend(id,vel,pos,perc):
+def postdronesend(jsondata):
     
+    r = requests.post('http://10.30.134.12:8011/api/drones', json=jsondata)  
 
-    r = requests.post('https://127.0.0.1:8011/api/drones', json={
-        "id": id,
-        "dueDate": datetime.now().strftime("%Y-%D-%M"),
-        "posizione": pos,
-        "velocita": vel,
-        "percentuale": perc
-    })
-    print(f"Status Code: {r.status_code}, Response: {r.json()}")
+def error():
+    print("errore orrore")
+
+
+
 
 #creo un metodo fittizio che simula il drone
 def dronedemo():
-    id=12223
+    
     velocita = random.randrange(30000,70000) / 1000
     posizione = random.randrange(30000,70000) / 1000
     percentuale = random.randrange(1000,100000) / 1000
-    dronesend(id,velocita,posizione,percentuale)
+    data=datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    idPersona = 1
+    idDrone = 1
+    print(data)
+    json={
+        "dueDate": data ,
+        "posizione": posizione,
+        "velocita": velocita,
+        "percentuale": percentuale,
+        "idPersona":idPersona,
+        "idDrone":idDrone
+    }
+    postdronesend(json)
+
+
+
 
 if __name__ == '__main__':
     #eseguo 
-    schedule.every(1).minutes.do(dronedemo())
+    schedule.every(1).minutes.do(dronedemo)
+    while True:
+           schedule.run_pending()
+           time.sleep(1)
+   
