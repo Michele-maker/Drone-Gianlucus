b033d33eaab5f9739c88e22cf4b56d7b9264df70
Michele <michele.sbrissa@stud.tecnicosuperiorekennedy.it>
2022-01-12 14:16:19 +0100

    indent client

--- client/ClientDrone.py
+++ client/ClientDrone.py
@@ -2,76 +2,78 @@ import requests
 import random
 import schedule
 from datetime import datetime
-import time 
+import time
 import json
 from paho.mqtt.client import Client
 
 
-HTTP= "ipserver.json"
-MQTT= "ipservermqtt.json"
-CLIENT = Client(client_id = "client_1")
+HTTP = "ipserver.json"
+MQTT = "ipservermqtt.json"
+CLIENT = Client(client_id="client_1")
+
 
 def getIpHttp():
-    f=open(HTTP,'r')
+    f = open(HTTP, 'r')
     xx = f.read()
     myjson = json.loads(xx)
     f.close()
     return myjson['ip']
+
 
 def getIpMqtt():
-    f=open(MQTT,'r')
+    f = open(MQTT, 'r')
     xx = f.read()
     myjson = json.loads(xx)
     f.close()
     return myjson['ip']
 
+
 def postdronesend(jsondata):
-    ip=getIpHttp()
-    r = requests.post(f'http://{ip}:8011/api/drones', json=jsondata)  
+    ip = getIpHttp()
+    r = requests.post(f'http://{ip}:8011/api/drones', json=jsondata)
+
 
 def error():
     print("errore orrore")
 
+
 def mqttdronepublish(jsondata):
-    ip=getIpMqtt()
-    #ip="10.30.134.17:1833"
-    #CLIENT.connect(ip)
+    ip = getIpMqtt()
+    # ip="10.30.134.17:1833"
+    # CLIENT.connect(ip)
     CLIENT.connect(ip, 1883, 60)
     print(jsondata["idDrone"])
     y = json.dumps(jsondata)
-    CLIENT.publish(topic = f"DroneGianlucus/{jsondata['idDrone']}",payload = y) 
-    
+    CLIENT.publish(topic=f"DroneGianlucus/{jsondata['idDrone']}", payload=y)
 
-#creo un metodo fittizio che simula il drone
+
+# creo un metodo fittizio che simula il drone
 def dronedemo():
-    
-    velocita = random.randrange(30000,70000) / 1000
-    posizione = random.randrange(30000,70000) / 1000
-    percentuale = random.randrange(1000,100000) / 1000
-    data=datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    idDrone = random.randrange(1,7) 
-    idPersona = random.randrange(1,20)
-    #print(data)
-    json={
-        "dueDate": data ,
-        "idPersona":idPersona,
-        "idDrone":idDrone,
+
+    velocita = random.randrange(30000, 70000) / 1000
+    posizione = random.randrange(30000, 70000) / 1000
+    percentuale = random.randrange(1000, 100000) / 1000
+    data = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    idDrone = random.randrange(1, 7)
+    idPersona = random.randrange(1, 20)
+    # print(data)
+    json = {
+        "dueDate": data,
+        "idPersona": idPersona,
+        "idDrone": idDrone,
         "posizione": posizione,
         "velocita": velocita,
         "percentuale": percentuale
-        
+
     }
 
     mqttdronepublish(json)
 
 
-
-
 if __name__ == '__main__':
-    #eseguo 
+    # eseguo
     getIpMqtt()
     schedule.every(10).seconds.do(dronedemo)
     while True:
-           schedule.run_pending()
-           time.sleep(1)
-   
+        schedule.run_pending()
+        time.sleep(1)
