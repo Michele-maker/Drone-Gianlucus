f32973457bff9008a9214c0e00dbdecf2dcbc82f
alemonteleale <alemonte07.am@gmail.com>
2022-01-19 12:43:05 +0100

    modifiche client

--- client/ClientDrone.py
+++ client/ClientDrone.py
@@ -5,12 +5,43 @@ from datetime import datetime
 import time
 import json
 from paho.mqtt.client import Client
+import logging
+import asyncio
 
+from aiocoap import *
+logging.basicConfig(level=logging.INFO)
+
+HOST='10.30.134.17'
+PATH='Drone' 
 
 HTTP = "ipserver.json"
 MQTT = "ipservermqtt.json"
 CLIENT = Client(client_id="client_1")
+#la funzione non va dato che ho riscontrato problemi con la libreria di coap per python
+async def postcoap(jsondata):
+    context = await Context.create_client_context()
+    HOST='10.30.134.17'
+    PATH='Ciao'
+    await asyncio.sleep(2)
+    payloadjson = json.dumps(jsondata)
+    request = Message(code=POST, payload=payloadjson, uri="coap://{HOST}/{PATH}")
 
+    response = await context.request(request).response
+
+    print('Result: %s\n%r'%(response.code, response.payload))
+#la funzione non va dato che ho riscontrato problemi con la libreria di coap per python
+async def getcoat():
+    protocol = await Context.create_client_context()
+
+    request = Message(code=GET, uri=f'coap://{HOST}/{PATH}')
+
+    try:
+        response = await protocol.request(request).response
+    except Exception as e:
+        print('Failed to fetch resource:')
+        print(e)
+    else:
+        print('Result: %s\n%r'%(response.code, response.payload))
 
 def getIpHttp():
     f = open(HTTP, 'r')
@@ -66,8 +97,8 @@ def dronedemo():
         "percentuale": percentuale
     }
 
-    mqttdronepublish(json)
-
+    #mqttdronepublish(json)
+    asyncio.run(postcoap())
 
 if __name__ == '__main__':
     # eseguo
